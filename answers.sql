810cf63860946f2210ac1e8eaed817f67eb9fd9a
leraTo20moloI <leratonthabiseng.moloi@gmail.com>
2025-05-09 11:28:41 +0200

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -1,28 +1,4 @@
-Q1
-SELECT checkNumber, paymentDate, amount 
-FROM payements;
 
---Q2
-SELECT orderDate, requiredDate, status 
-FROM orders
-WHERE status = 'In Process'
-ORDER BY orderDate DESC;
-
---Q3
-SELECT firstName, lastName, email
-FROM employees
-WHERE jobTitle = 'Sales Rep'
-ORDER BY employeeNumber DESC;
-
---Q4
-SELECT *
-FROM offices;
-
---Q5
-SELECT prouctName, quantityInStock
-FROM products
-ORDER BY buyPrice ASC
-SELECT TOP 5 buyprice FROM product;
 
 
 
